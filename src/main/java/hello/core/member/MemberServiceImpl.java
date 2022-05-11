f6001b63b5e952ff576439e2821ad34aa7be6793
한상선 <hs94122@naver.com>
2022-05-11 21:26:10 +0900

    2022-05-11 21:26 스프링 공부

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //인터페이스 변수명 = new 클래스();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
