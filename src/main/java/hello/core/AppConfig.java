e7d501d2a1fd09c3c38be6c01f44b8b513d93e92
한상선 <hs94122@naver.com>
2022-05-22 21:13:58 +0900

    study 스프링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,7 +1,6 @@
 package hello.core;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
@@ -9,26 +8,32 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration//애플리케이션의 구성정보, 설정정보를 담당하는 파일이다 선언
 public class AppConfig {
-
     //AppConfig 는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성
     //생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입함
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
         //memberServiceImpl은 필드에서 new를 하지 않고, 생성자를 통해서 할당이 됨
         //-> 생성자 주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,14 +3,17 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     //psvm치고 엔터
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();//memberServiceImpl을 만들어 줌
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();//memberServiceImpl을 만들어 줌
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);//AppConfig에 있는 설정정보를 가지고, 스프링에서 관리를 해줌
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
